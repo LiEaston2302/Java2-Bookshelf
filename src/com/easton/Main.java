724e0b6703b264a62213b5b00ec5b94e1114d767
Liam Easton <liam-easton@student.kirkwood.edu>
2021-11-03 13:23:12 -0500

    Add Book class

--- src/com/easton/Main.java
+++ src/com/easton/Main.java
@@ -3,7 +3,8 @@ package com.easton;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hi");
-        
+        Book b1 = new Book("Java Generics", "Liam Easton");
+        Book b2 = new Book("Java Collections", "Liam Easton");
+        Book b3 = new Book("Java Collections", "Jacob Rugger");
     }
 }
