8e697e1be3b638306814d4cb0933f06e11635354
Jacob Lee Rugger <k0765056@kirkwood.edu>
2021-11-03 13:22:59 -0500

    Add ChildrensBook class

--- src/com/easton/Main.java
+++ src/com/easton/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hi");
-        
+
+        Book b4 = new ChildrensBook("Java for Kids", "Jacob", "Liam");
     }
 }
